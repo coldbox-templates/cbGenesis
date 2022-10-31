cca3d1e5c00f3b58e3e4779a0f555e0cd154056c
Luis Majano <lmajano@gmail.com>
2022-10-31 11:07:44 +0100

    first import

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+const defaultTheme = require( "tailwindcss/defaultTheme" );
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content : [
+		// App resources
+		"./resources/**/*.vue",
+		"./resources/**/*.js",
+		// App Layouts and views
+		"./layouts/*.cfm",
+		"./views/*.cfm",
+		// Any Module Views
+		"./modules_app/**/*.cfm",
+		// Any model templates
+		"./models/**/*.cfm"
+	],
+	theme : {
+		extend : {
+			fontFamily : {
+				sans : [
+					"Source Sans",
+					...defaultTheme.fontFamily.sans
+				],
+				mono : [
+					"Menlo",
+					"Monaco",
+					"Consolas",
+					"Liberation Mono",
+					"Courier New",
+					...defaultTheme.fontFamily.mono
+				],
+				serif : [
+					"Georgia",
+					"Cambria",
+					"Times New Roman",
+					...defaultTheme.fontFamily.serif
+				]
+	  		},
+		}
+	},
+	plugins : [
+		require( "@tailwindcss/forms" ),
+		require( "@tailwindcss/typography" ),
+		require( "@tailwindcss/aspect-ratio" )
+	]
+};
